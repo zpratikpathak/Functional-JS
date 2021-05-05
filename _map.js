7a14125043eee2b0e096033be7aa80d76a6f25a1
Pratik Pathak <pratikpathak200@gmail.com>
2021-05-05 18:32:47 +0530

    Map return a array

--- _map.js
+++ _map.js
@@ -1,6 +1,6 @@
 // npm install underscore
 const _= require('underscore')
-/*
+
 _.map([1,2,3,4], function(v,i,list){ //v=items, i=index, list is list
     console.log(v,i,list)
 })
@@ -17,12 +17,19 @@ _.map([1,2,3,4], function(list){
 // 2
 // 3
 // 4
-*/
 
+//  //test Skill
 const weapons = ['candlestick', 'lead pipe', 'revolver'];
-
 const returnWep = (list)=>{
     console.log(list)
 }
-
 _.map(weapons,returnWep)
+
+//Alternate Method
+const makeBroken = function(item){
+    return `broken ${item}`;
+  };
+const brokenWeapons = _.map(weapons, makeBroken);
+console.log(brokenWeapons)
+//[ 'broken candlestick', 'broken lead pipe', 'broken revolver' ]
+//Note: _.map returns a array
