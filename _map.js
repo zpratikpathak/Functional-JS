12bc5d1d5bace595fa57c93b9ee8b0891018ce02
Pratik Pathak <pratikpathak200@gmail.com>
2021-05-05 18:28:18 +0530

    Iterated using _.map

--- _map.js
+++ _map.js
@@ -1,10 +1,28 @@
 // npm install underscore
 const _= require('underscore')
-
+/*
 _.map([1,2,3,4], function(v,i,list){ //v=items, i=index, list is list
     console.log(v,i,list)
 })
+// 1 0 [ 1, 2, 3, 4 ]
+// 2 1 [ 1, 2, 3, 4 ]
+// 3 2 [ 1, 2, 3, 4 ]
+// 4 3 [ 1, 2, 3, 4 ]
+_.
 
 _.map([1,2,3,4], function(list){
     console.log(list)
 })
+// 1
+// 2
+// 3
+// 4
+*/
+
+const weapons = ['candlestick', 'lead pipe', 'revolver'];
+
+const returnWep = (list)=>{
+    console.log(list)
+}
+
+_.map(weapons,returnWep)
