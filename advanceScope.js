6e1697de7e64ec356ddec44fbe7295e453c95c6f
Pratik Pathak <pratikpathak200@gmail.com>
2021-05-07 20:04:53 +0530

    Asynchronus, Closure

--- advanceScope.js
+++ advanceScope.js
@@ -1,10 +1,22 @@
-const myAlert = () => {
+// const myAlert = () => {
+//     const x = "Help! I think I found a clue!";
+//     const alerter = () => {
+//       console.log(x);
+//     };
+//     alerter();
+//   };
+// //   alerter() Not accessible Since Scope is withing the myAlert
+//   myAlert()
+  
+  const myAlert = () => {
     const x = "Help! I think I found a clue!";
-    const alerter = () => {
+    const alerter = ()=>{
       console.log(x);
     };
-    alerter();
-  };
-//   alerter() Not accessible Since Scope is withing the myAlert
-  myAlert()
   
+    setTimeout(alerter, 1000) // Asyncronous function, executed after logging
+    console.log('what happens first? this log or the alert?')
+  };
+  myAlert()
+// what happens first? this log or the alert?
+// Help! I think I found a clue!
